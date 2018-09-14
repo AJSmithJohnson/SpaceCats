4109e8141c5d25980f2fa24ca319559df47cf2f1
Nick <pattison.nick@gmail.com>
2018-09-14 19:15:03 -0400

    Added project working files

---
+++ Assets/PlaneLock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneLock : MonoBehaviour {
+
+    public Plane plane;
+
+    void Start()
+    {
+        SetAngle(-90);
+    }
+
+    /// <summary>
+    /// Sets the plane that this object is locked to.
+    /// </summary>
+    /// <param name="yaw">The yaw angle to orient the plane (in degrees).</param>
+    public void SetAngle(float yaw)
+    {
+        yaw *= Mathf.Deg2Rad;
+        Vector3 normal = new Vector3();
+        normal.z = Mathf.Sin(yaw);
+        normal.x = Mathf.Cos(yaw);
+        plane = new Plane(normal, transform.position);
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + plane.normal);
+    }
+
+}
