51701c922c21b6aee35ca764c328bf389247a7ea
Josh <haneryu@gmail.com>
2018-09-15 13:17:25 -0400

    Fixed a few things, rotated the level

---
+++ Assets/PauseCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseCommand : MonoBehaviour {
+
+    bool paused;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+            } else
+            if (Time.timeScale == 0) Time.timeScale = 1;
+        }
+	}
+
+    public void Unpause() {
+        Time.timeScale = 1;
+    }
+
+}
