3c20ac424ff464e37e6af92fcb7ed88195e2a5a2
Josh <haneryu@gmail.com>
2018-09-15 07:56:17 -0400

    Added in targets to save, along with finish line and win condition

---
+++ Assets/Scripts/Cats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cats : MonoBehaviour
+{
+    int numberOfCats = 0;
+    int totalCats;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[] cat;
+        cat = GameObject.FindGameObjectsWithTag("Cat");
+        foreach(GameObject c in cat)
+        {
+            totalCats++;
+        }
+        Debug.Log("Number to save " + totalCats);
+    }
+
+    public void GetCat()
+    {
+        numberOfCats += 1;
+        Debug.Log(numberOfCats);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (numberOfCats == totalCats)
+            {
+                Debug.Log("YOU WIN!!");
+            }
+            else
+            {
+                Debug.Log("Save all the cats and return to escape!");
+            }
+        }
+    }
+}
