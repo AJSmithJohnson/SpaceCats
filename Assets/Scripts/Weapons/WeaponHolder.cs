67254ca9a0f42c51c94937f2af33c8922f87e1f8
Josh <haneryu@gmail.com>
2018-09-15 09:00:42 -0400

    more guns and sprites added in

--- Assets/Scripts/Weapons/WeaponHolder.cs
+++ Assets/Scripts/Weapons/WeaponHolder.cs
@@ -14,14 +14,19 @@ public class WeaponHolder : MonoBehaviour {
 	void Start () {
         body = GetComponent<Rigidbody>();
         //weapon = GetComponentInChildren<Weapon>();
-        weapon1 = this.transform.GetChild(0).GetComponent<Weapon>();
-        weapon2 = this.transform.GetChild(1).GetComponent<Weapon>();
-        weapon2 = this.transform.GetChild(2).GetComponent<Weapon>();
+        weapon1 = transform.Find("KittenCannon").GetComponent<Weapon>();
+        weapon2 = transform.Find("Cannon").GetComponent<Weapon>();
+        //weapon2 = transform.FindChild("Laser").GetComponent<Weapon>();
+       weapon3 = transform.Find("Laser").GetComponent<Weapon>();
 
-        currentWeapon = weapon1;
+        SetWeapon(1);
     }
 	
 	void Update () {
+
+        if (Input.GetKeyDown("1")) SetWeapon(1);
+        if (Input.GetKeyDown("2")) SetWeapon(2);
+        if (Input.GetKeyDown("3")) SetWeapon(3);
 
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         Plane plane = new Plane(-transform.forward, transform.position);
@@ -43,5 +48,32 @@ public class WeaponHolder : MonoBehaviour {
             float thrust = currentWeapon.Shoot(-thrustDir);
             body.AddForce(thrustDir * thrust, ForceMode.Impulse);
         }
-	}
+
+    }
+
+    void SetWeapon(int x)
+    {
+        Debug.Log("Switching to " + x);
+        switch (x)
+        {
+            case 1:
+                currentWeapon = weapon1;
+                weapon1.GetComponentInParent<SpriteRenderer>().enabled = true;
+                weapon2.GetComponentInParent<SpriteRenderer>().enabled = false;
+                weapon3.GetComponentInParent<SpriteRenderer>().enabled = false;
+                break;
+            case 2:
+                currentWeapon = weapon2;
+                weapon1.GetComponentInParent<SpriteRenderer>().enabled = false;
+                weapon2.GetComponentInParent<SpriteRenderer>().enabled = true;
+                weapon3.GetComponentInParent<SpriteRenderer>().enabled = false;
+                break;
+            case 3:
+                currentWeapon = weapon3;
+                weapon1.GetComponentInParent<SpriteRenderer>().enabled = false;
+                weapon2.GetComponentInParent<SpriteRenderer>().enabled = false;
+                weapon3.GetComponentInParent<SpriteRenderer>().enabled = true;
+                break;
+        }
+    }
 }
