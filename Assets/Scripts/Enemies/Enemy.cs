781ebe43d820403904e23a9bc7fcc97c6611af1d
Unknown <smithja@ferris.edu>
2018-09-14 20:04:43 -0400

    Set Up a follower Enemy
    Enemy follows player now

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public float health;
+    public float damage;
+    public float speed;
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Enemies/FollowerEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerEnemy : Enemy {
+
+    void Start()
+    {
+        health = 50;
+        damage = 20;
+        speed = 2;
+        target = GameObject.Find("Cube");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 chaseTarget = target.transform.position - this.transform.position;
+        this.transform.position += chaseTarget * .01f;
+       
+    }
+}
