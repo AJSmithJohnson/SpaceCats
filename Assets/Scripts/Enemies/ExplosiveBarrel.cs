adca89af5f5d4ceff2a8911501d14c38d0518a51
Unknown <smithja@ferris.edu>
2018-09-15 04:18:19 -0400

    Added Explosive Barrels and Explosions
    Multiple prefab objects added for ease of use

---
+++ Assets/Scripts/Enemies/ExplosiveBarrel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : Enemy {
+
+    public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player");
+        boxCol = GetComponent<BoxCollider>();
+        body = GetComponent<Rigidbody>();
+        health = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        body.velocity = Vector3.zero;
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        
+
+    }
+
+
+    public override void CheckForDeath()
+    {
+       
+            
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+  
+}
