7c1f7a3f8e1b767ec80731727587935b686b06b0
Nick <pattison.nick@gmail.com>
2018-09-14 20:59:17 -0400

    Added features to Projectile and Weapon classes
    - Deleted a bunch of extra files in /SpaceCats
    * Moved Assets/Projectile to Assets/Scripts/Projectile
    + Added documentation comments to Projectile and Weapon
    + Added a lifespan to Projectiles; they auto-die when they get too old
    + Added a countdown timer to Weapon

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    /// <summary>
+    /// The desired speed of the projectile. When this projectile is
+    /// initialized, this value is multiplied by the desired direction.
+    /// </summary>
+    public float speed = 10;
+    /// <summary>
+    /// The maximum lifespan of this projectile, in seconds. The projectile
+    /// will auto-destroy after this lifespan has passed.
+    /// </summary>
+    public float lifespan = 2;
+    /// <summary>
+    /// The current age (in seconds) of this projectile. Compared to lifespan each frame.
+    /// </summary>
+    private float age = 0;
+    /// <summary>
+    /// The RigidBody component of this projectile. 
+    /// </summary>
+    private Rigidbody _body;
+    private Rigidbody body {
+        get
+        {
+            if (!_body) _body = GetComponent<Rigidbody>();
+            return _body;
+        }
+    }
+    /// <summary>
+    /// This should be called after spawning to set
+    /// the desired direction of the projectile.
+    /// </summary>
+    /// <param name="dir">A directional vector. It should be normalized!</param>
+	public void Init (Vector3 dir) {
+        body.velocity = dir * 10;
+        // Maybe we want to get rid of this, rotate the object on spawn,
+        // and set initial velocity in Start()
+	}
+    /// <summary>
+    /// Update the age of the projectile, die of old age...
+    /// </summary>
+    void Update()
+    {
+        age += Time.deltaTime;
+        if(age >= lifespan)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,26 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour {
-
+    /// <summary>
+    /// The projectile to spawn when this Weapon shoots.
+    /// </summary>
     public Projectile prefabProjectile;
-
+    /// <summary>
+    /// The amount of recoil force this Weapon applies to the character shooting.
+    /// </summary>
     public float forceKickback = 100;
+    /// <summary>
+    /// The maximum ammo this Weapon can hold.
+    /// </summary>
     public float ammoMax = 10;
+    /// <summary>
+    /// The cooldown time between shots, in seconds.
+    /// </summary>
+    public float bulletDelay = .1f;
+    /// <summary>
+    /// The current amount of ammo this weapon has.
+    /// </summary>
     float ammo;
+    /// <summary>
+    /// The current amount of time (in seconds) to wait before this weapon can be shot again.
+    /// </summary>
+    float bulletDelayCurrent = 0;
     
     void Start()
     {
         ammo = ammoMax;
     }
-
+    /// <summary>
+    /// Counts down the shooting cooldown.
+    /// </summary>
+    void Update()
+    {
+        if(bulletDelayCurrent > 0)
+        {
+            bulletDelayCurrent -= Time.deltaTime;
+        }
+    }
+    /// <summary>
+    /// Attempts to shoot the weapon. Only shoots if there is enough ammo
+    /// and if the shooting cooldown has counted down.
+    /// </summary>
+    /// <param name="dir">The direction to shoot the gun.</param>
+    /// <returns>The amount of force to apply to the character shooting the weapon.</returns>
     public float Shoot(Vector3 dir)
     {
         if (ammo <= 0) return 0;
-        // TODO: check cooldown
+        if (bulletDelayCurrent > 0) return 0;
+
+        bulletDelayCurrent = bulletDelay;
+        ammo--;
 
         Projectile p = Instantiate(prefabProjectile, transform.position, Quaternion.identity);
         p.Init(dir);
-
+        
         return forceKickback;
     }
 }
