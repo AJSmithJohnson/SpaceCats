f9195fb1dfa7a4ac4fecf0799fdada4057f38154
Nick <pattison.nick@gmail.com>
2018-09-14 23:41:25 -0400

    Removed PlaneLock
    Removed PlaneLock component from the game.

--- Assets/Scripts/Weapons/WeaponHolder.cs
+++ Assets/Scripts/Weapons/WeaponHolder.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class WeaponHolder : MonoBehaviour {
 
     public Camera cam;
-    private PlaneLock planeLock;
     private Rigidbody body;
     private Weapon weapon;
 
 	void Start () {
-        planeLock = GetComponent<PlaneLock>();
         body = GetComponent<Rigidbody>();
         weapon = GetComponentInChildren<Weapon>();
 	}
@@ -18,12 +16,12 @@ public class WeaponHolder : MonoBehaviour {
 	void Update () {
 
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.back, transform.position);
+        Plane plane = new Plane(-transform.forward, transform.position);
         float dis;
 
         Vector3 thrustDir = Vector3.zero;
 
-        if (planeLock.plane.Raycast(ray, out dis))
+        if (plane.Raycast(ray, out dis))
         {
             Vector3 pos = ray.GetPoint(dis);
             Debug.DrawLine(transform.position, pos);
