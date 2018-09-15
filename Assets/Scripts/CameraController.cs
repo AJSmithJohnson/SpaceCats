5533d4661f68fafa8b83dd43b69e2e5af7560c8a
Nick <pattison.nick@gmail.com>
2018-09-14 23:07:01 -0400

    Added a CameraController script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float easeMultiplier = 10;
+    public Transform target;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * easeMultiplier);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, Time.deltaTime * easeMultiplier);
+        }
+	}
+}
